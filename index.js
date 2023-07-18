b3846caa5d08589bedabaab255596e06d071b790
Jean Lucca <jefreecs.silva@gmail.com>
2023-07-18 12:57:32 -0300

    add Model User

--- index.js
+++ index.js
@@ -8,19 +8,37 @@ const jwl = require('jsonwebtoken')
 
 app.use(express.json())
 
+//models
+
+const User = require('./models/User')
+
+
 app.get('/', (req,res)=> {
     res.status(200).json({
         msg: "jnx API"
     })  
 })  
+app.post('/auth/register', async (req,res)=> {
+
+    const {name, email, password, confirmPassword} = req.body
+
+    if(!name || !email || !password || !confirmPassword){
+        res.status(422).json({mgs: 'Some fields are missing'})
+    }
+    if(password != confirmPassword){
+        res.status(422).json({mgs: 'passwords are not matching'})
+    }
+    else res.status(200).json({mgs: 'passed'})
+
+})
 const dbUser = process.env.DB_USER
 const dbPassword = process.env.DB_PASS
 
-mongoose.connect(`mongodb+srv://jefreecs:${dbPassword}@cluster0.nncpcxu.mongodb.net/?retryWrites=true&w=majority`).then(()=>{
+mongoose.connect(`mongodb+srv://${dbUser}:${dbPassword}@cluster0.nncpcxu.mongodb.net/?retryWrites=true&w=majority`).then(()=>{
     app.listen(8080)
     console.log("connected in MongoDb")
 }).catch((error)=>{console.log(`Error:${error}`)})
 
-app.listen(8080)
+
 
 
