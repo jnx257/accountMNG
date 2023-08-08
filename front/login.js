fce0a3ee191c733c15db74950b74d8f71f5a3b87
Jean Lucca <jefreecs.silva@gmail.com>
2023-08-07 21:46:37 -0300

    login fixed

--- front/login.js
+++ front/login.js
@@ -3,22 +3,15 @@ async function loginDB() {
   const password = document.getElementById("password").value;
   const body = JSON.stringify({ email: email, password: password });
 
-  console.log(body);
-
-  try {
-    const response = await fetch("http://localhost:8080/auth/login", {
-      method: "POST",
-      body: body,
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-
-    const data = await response.json();
-    console.log("connected in DB", data);
-  } catch (err) {
-    console.log(`error: ${err}`);
-  }
+  fetch('http://localhost:8080/auth/login',
+  {method: "POST",
+   body: body,
+   headers: {
+    "Content-Type": "application/json",
+  }})
+  .then((response)=> response.json)
+  .then(data => console.log("login auth", data))
+  .catch((error)=> console.log(error))
 }
 
 document.getElementById("loginButton").addEventListener("click", loginDB);
