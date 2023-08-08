ed9f0c7642cc128a757682cfa362535916894801
Jean Lucca <jefreecs.silva@gmail.com>
2023-08-08 07:29:46 -0300

    login fixed

--- front/login.js
+++ front/login.js
@@ -3,18 +3,21 @@ async function loginDB() {
   const password = document.getElementById("password").value;
   const body = JSON.stringify({ email: email, password: password });
 
-  fetch('http://localhost:8080/auth/login',
-  {method: "POST",
-   body: body,
-   headers: {
-    "Content-Type": "application/json",
-  }})
-  .then((response)=> response.json)
-  .then(data => {console.log("login auth", data)
-  const loginPopup = document.querySelector('.PopUp')
-  loginPopup.style.display = 'block';
-})
-  .catch((error)=> console.log(error))
+  fetch("http://localhost:8080/auth/login", {
+    method: "POST",
+    body: body,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => response.json)
+    .then((data) => {
+      console.log("login auth", data);
+      const loginPopup = document.querySelector(".PopUp");
+      loginPopup.style.display = "block";
+      setTimeout(() => (loginPopup.style.display = "none"), 5000);
+    })
+    .catch((error) => console.log(error));
 }
 
 document.getElementById("loginButton").addEventListener("click", loginDB);
